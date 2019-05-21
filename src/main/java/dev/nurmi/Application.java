06902a0e37df2b5e6806dbcc13ed376b680a7aca
vellu <nurmi.vp@gmail.com>
2019-05-21 20:38:20 +0300

    profile configurations files for testing

--- src/main/java/dev/nurmi/Application.java
+++ src/main/java/dev/nurmi/Application.java
@@ -1,13 +1,34 @@
 package dev.nurmi;
 
+import java.util.Arrays;
+
+import javax.swing.Spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Application {
-    
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-    
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	private Environment environment;
+
+	@Value("${var.profile}")
+	private String profile;
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void doSomethingAfterStartup() {
+		System.out.println("Active profiles: " + Arrays.toString(this.environment.getActiveProfiles()));
+		System.out.println("Congfiguration file: " + profile);
+	}
+
 }
