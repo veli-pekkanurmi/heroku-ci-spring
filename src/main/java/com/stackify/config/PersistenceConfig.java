d7a3df8bd25891b7408058855c3b77bc29f9da7e
vellu <nurmi.vp@gmail.com>
2019-05-21 19:57:33 +0300

    h2 changes

--- src/main/java/com/stackify/config/PersistenceConfig.java
+++ src/main/java/com/stackify/config/PersistenceConfig.java
@@ -11,14 +11,14 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 @Configuration
 public class PersistenceConfig {
 
-    @Bean
-    public DataSource dataSource() {
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase db = builder
-        		.setType(EmbeddedDatabaseType.H2)
-        		.addScript("mySchema.sql")
-        		.addScript("myData.sql").build();
-        return db;
-    }
+//    @Bean
+//    public DataSource dataSource() {
+//        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+//        EmbeddedDatabase db = builder
+//        		.setType(EmbeddedDatabaseType.H2)
+//        		.addScript("mySchema.sql")
+//        		.addScript("myData.sql").build();
+//        return db;
+//    }
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into employee(name) values ('Mikko');
+insert into employee(name) values ('Anna');
+insert into employee(name) values ('Tapio');
